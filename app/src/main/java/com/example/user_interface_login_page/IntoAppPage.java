6d92f8728119889eb0344b6cd0c325480c362b58
EliasCoumine <e.coumine@gmail.com>
2024-10-09 00:25:59 -0400

    Functionality for Sign Out
    Added the functionality for the sign out

--- app/src/main/java/com/example/user_interface_login_page/IntoAppPage.java
+++ app/src/main/java/com/example/user_interface_login_page/IntoAppPage.java
@@ -1,6 +1,8 @@
 package com.example.user_interface_login_page;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class IntoAppPage extends AppCompatActivity {
+
+    private Button buttonSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,13 @@ public class IntoAppPage extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void initializeEventListeners() {
+        buttonSignOut.setOnClickListener(v -> {
+            Intent intent = new Intent(IntoAppPage.this, InitialPage.class);
+            startActivity(intent);
         });
     }
 }
