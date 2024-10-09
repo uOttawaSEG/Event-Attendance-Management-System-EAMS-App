84ae172941a6957ef5522d2064e4a09657ff2ad1
RodricDib06 <rodric.dib@gmail.com>
2024-10-09 17:15:35 -0400

    Created a Validator class and implemented the Attendee class

--- app/src/main/java/com/example/user_interface_login_page/Attendee.java
+++ app/src/main/java/com/example/user_interface_login_page/Attendee.java
@@ -1,9 +1,13 @@
 package com.example.user_interface_login_page;
 
-public class Attendee extends User {
-
-    public Attendee(String firstName, String lastName, String emailAddress, String accountPassword, String address, String phoneNumber) {
+public class Attendee extends User{
+    public Attendee(String firstName, String lastName, String emailAddress, String accountPassword, String address, String phoneNumber, String organizationName) {
         super(firstName, lastName, emailAddress, accountPassword, address, phoneNumber);
+        Validator.validateInputAttendee(firstName, lastName, emailAddress, accountPassword, address, phoneNumber);
+
+
     }
 
 }
+
+
