b8d2ad46cb6c3a8f67d70e640b0e5fa3b9ea52f2
Anthony Alam <anthony.alam2005@gmail.com>
2024-10-15 19:27:47 -0400

    Added constants for regex patterns

--- app/src/main/java/com/example/user_interface_login_page/Validator.java
+++ app/src/main/java/com/example/user_interface_login_page/Validator.java
@@ -2,8 +2,13 @@ package com.example.user_interface_login_page;
 
 public class Validator {
 
+    private static final String FIRSTNAME_PATERN = "[a-zA-Z]+";
+    private static final String LASTNAME_PATERN = "[a-zA-Z]+";
+    private static final String EMAIL_ADDRESS_PATERN = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+    private static final String ACCOUNT_PASSWORD_PATERN = "^.{8,}$";
+    private static final String PHONENUMBER_PATTERN = "^\\+?[0-9. ()-]{7,15}$";
     private static final String ADDRESS_PATTERN = "^\\d+\\s[A-z]+(?:\\s[A-z]+)*(?:\\s(?:Apt|Unit|Suite)\\s\\d+)?(?:,\\s[A-z]+)*(?:,\\s[A-Z]{2})?(?:\\s\\d{5})?$";
-    private static final String PHONE_PATTERN = "^\\+?[0-9. ()-]{7,15}$";
+    private static final String ORGANIZATIONNAME_PATERN = "^[a-zA-Z0-9]+$";
 
     /**
      * Validate the first name
@@ -11,7 +16,7 @@ public class Validator {
      * @return true if it is valid, otherwise false
      */
     public static boolean validateFirstName(String firstName){
-        return !firstName.isEmpty() && firstName.length() >= 2 && firstName.matches("[a-zA-Z]+");
+        return !firstName.isEmpty() && firstName.length() >= 2 && firstName.matches(FIRSTNAME_PATERN);
     }
 
     /**
@@ -20,7 +25,7 @@ public class Validator {
      * @return true if it is valid, otherwise false
      */
     public static boolean validateLastName(String lastName){
-        return !lastName.isEmpty() && lastName.length() >= 2 && lastName.matches("[a-zA-Z]+");
+        return !lastName.isEmpty() && lastName.length() >= 2 && lastName.matches(LASTNAME_PATERN);
     }
 
     /**
@@ -29,7 +34,7 @@ public class Validator {
      * @return true if the email address is valid, false otherwise
      */
     public static boolean validateEmail (String emailAddress){
-        return !emailAddress.isEmpty() && emailAddress.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$");
+        return !emailAddress.isEmpty() && emailAddress.matches(EMAIL_ADDRESS_PATERN);
 
     }
 
@@ -39,7 +44,7 @@ public class Validator {
      * @return true if the phone number is valid, otherwise false
      */
     public static boolean validatePhoneNumber (String phoneNumber){
-        return !phoneNumber.isEmpty() && phoneNumber.matches(PHONE_PATTERN);
+        return !phoneNumber.isEmpty() && phoneNumber.matches(PHONENUMBER_PATTERN);
     }
 
     /**
@@ -70,7 +75,7 @@ public class Validator {
      * @return true if the account password is true, otherwise false
      */
     public static boolean validatePassword(String accountPassword) {
-        return !accountPassword.isEmpty() && accountPassword.length()>=8;
+        return !accountPassword.isEmpty() && accountPassword.length()>=8 && accountPassword.matches(ACCOUNT_PASSWORD_PATERN);
 
     }
 
@@ -80,7 +85,7 @@ public class Validator {
      * @return true if the organization is valid, otherwise false
      */
     public static boolean validateOrganizationName(String organizationName) {
-        return !organizationName.isEmpty() && organizationName.matches("^[a-zA-Z0-9]+$");
+        return !organizationName.isEmpty() && organizationName.matches(ORGANIZATIONNAME_PATERN);
     }
 
 
