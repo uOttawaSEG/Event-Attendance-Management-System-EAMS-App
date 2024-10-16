abc51130984f57eea7c8ab2d2fae0d3a2eff7477
Daniel <dlabib2005@gmail.com>
2024-10-15 23:37:53 -0400

    fixed small error in admin constructor

--- app/src/main/java/com/example/user_interface_login_page/Administrator.java
+++ app/src/main/java/com/example/user_interface_login_page/Administrator.java
@@ -1,8 +1,8 @@
 package com.example.user_interface_login_page;
 
 public class Administrator extends User {
-    public Administrator(String firstName, String lastName, String emailAddress, String accountPassword, String address, String phoneNumber) {
-        super(firstName, lastName, emailAddress, accountPassword, address, phoneNumber);
+    public Administrator(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address) {
+        super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address);
         setUserType("Administrator");
     }
 }
